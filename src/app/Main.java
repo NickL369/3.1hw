3094a76d948522ce81cf6081de1332772d13e98d
nlobachev <nikita.lobachev@fintech-farm.com>
2025-02-10 23:29:27 +0200

    Add Celsius to Fahrenheit conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,9 +7,17 @@ public class Main {
         double fahrenheit = 98.6;
         double celsius = convertFahrenheitToCelsius(fahrenheit);
         System.out.println(fahrenheit + "°F is " + celsius + "°C");
+
+        double celsiusValue = 37;
+        double fahrenheitConverted = convertCelsiusToFahrenheit(celsiusValue);
+        System.out.println(celsiusValue + "°C is " + fahrenheitConverted + "°F");
     }
 
     private static double convertFahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
+    }
+
+    private static double convertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 }
